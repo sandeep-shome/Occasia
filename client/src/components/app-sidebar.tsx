98ee4a54f600059b7206b94ae92adb8a54eb7c8f
sandeep <sandeepshome10@gmail.com>
2025-05-09 20:37:41 +0530

    Chore: remove unused code 🔥

--- client/src/components/app-sidebar.tsx
+++ client/src/components/app-sidebar.tsx
@@ -1,7 +1,5 @@
 import * as React from "react";
 import { ChevronRight } from "lucide-react";
-
-import { SearchForm } from "@/components/search-form";
 import { VersionSwitcher } from "@/components/version-switcher";
 import {
   Collapsible,
