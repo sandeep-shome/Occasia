09f0528a95bfed0cd972917af2b2e11eb68cec74
sandeep <sandeepshome10@gmail.com>
2025-05-09 19:12:00 +0530

    Chore: remove unusable code from layout 🔥

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -36,15 +36,6 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header>
           {children}
         </body>
       </html>
