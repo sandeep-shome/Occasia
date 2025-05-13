71460598445668bbf802bacd13ed8ee07a0cc05e
sandeep <sandeepshome10@gmail.com>
2025-05-13 22:18:07 +0530

    Chore: add user register route supporting clerk webhook ✨

---
+++ client/src/app/api/webhook/register/route.ts
@@ -0,0 +1,75 @@
+import { NextRequest } from "next/server";
+import { Webhook } from "svix";
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { headers } from "next/headers";
+import { prisma } from "@/lib/prisma";
+
+export async function POST(req: NextRequest) {
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  if (!WEBHOOK_SECRET) throw new Error("webhook secret is not defined");
+
+  const headerPayload = await headers();
+  const svix_id = headerPayload.get("svix-id");
+  const svix_timestamp = headerPayload.get("svix-timestamp");
+  const svix_signature = headerPayload.get("svix-signature");
+
+  if (!svix_id || !svix_timestamp || !svix_signature)
+    return new Response("Something went wrong with the svix headers", {
+      status: 405,
+    });
+  const payload = JSON.stringify(await req.json());
+
+  const wh = new Webhook(WEBHOOK_SECRET);
+  let evt: WebhookEvent;
+
+  try {
+    evt = wh.verify(payload, {
+      "svix-id": svix_id,
+      "svix-signature": svix_signature,
+      "svix-timestamp": svix_timestamp,
+    }) as WebhookEvent;
+  } catch (error) {
+    return new Response("Something went wrong with the svix verification", {
+      status: 405,
+    });
+  }
+
+  const evtType = evt.type;
+
+  if (evtType === "user.created") {
+    try {
+      const {
+        email_addresses,
+        primary_email_address_id,
+        first_name,
+        last_name,
+      } = evt.data;
+      const primaryEmailAddress = email_addresses.find(
+        (email) => email.id === primary_email_address_id
+      )?.email_address;
+      const fullName = first_name?.concat(last_name || "");
+
+      if (!primaryEmailAddress) {
+        return new Response("Primary email not found", {
+          status: 404,
+        });
+      }
+
+      const user = await prisma.user.create({
+        data: {
+          email: primaryEmailAddress,
+          name: fullName || "",
+        },
+      });
+      return new Response(JSON.stringify(user), { status: 201 });
+    } catch (error) {
+      return new Response("Something went wrong while creating user", {
+        status: 405,
+      });
+    }
+  } else {
+    return new Response("Something went wrong with the event type", {
+      status: 405,
+    });
+  }
+}
