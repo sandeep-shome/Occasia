127bc7568714be5d4c582ae56979993cfc3bf551
sandeep <sandeepshome10@gmail.com>
2025-05-15 11:47:33 +0530

    Chore: refactor speech id generation api ♻️

--- client/src/app/api/generation/create/route.ts
+++ client/src/app/api/generation/create/route.ts
@@ -1,3 +1,4 @@
+import { prisma } from "@/lib/prisma";
 import { NextRequest } from "next/server";
 
 interface Payload {
@@ -9,9 +10,33 @@ interface Payload {
 }
 
 export async function POST(req: NextRequest) {
-  const payload = (await req.json()) as Payload;
+  try {
+    const payload = (await req.json()) as Payload;
+    const userData = await prisma.user.findUnique({
+      where: {
+        id: payload?.userId,
+      },
+    });
+    if (!userData) {
+      return new Response("User not found!", { status: 404 });
+    }
 
-  if (!payload) {
-    return new Response("payload not found!", { status: 405 });
+    if (userData.tokens < 1) {
+      return new Response("Ran out of tokens", { status: 405 });
+    }
+
+    const { id: speechId } = await prisma.speech.create({
+      data: {
+        userId: payload.userId,
+        name: payload.name,
+        internalPrompt: payload.internalPrompt,
+        generalPrompt: payload.generalPrompt,
+        duration: payload.duration,
+      },
+    });
+
+    return new Response(JSON.stringify({ speechId }), { status: 201 });
+  } catch (error) {
+    return new Response("Something went wrong!", { status: 500 });
   }
 }
