bfb537d1566e96c3241af4b98fb36ad32b0efd65
sandeep <sandeepshome10@gmail.com>
2025-05-17 10:04:58 +0530

    Chore: wrap the component with store provider

--- client/src/components/app-sidebar.tsx
+++ client/src/components/app-sidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import * as React from "react";
 import { ChevronRight } from "lucide-react";
 import {
@@ -28,16 +30,15 @@ import {
 import Link from "next/link";
 import Image from "next/image";
 import logo from "@/assets/logo.svg";
-import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { Progress } from "./ui/progress";
+import StoreProvider from "@/lib/store-provider";
 
 // This is sample data.
 const data = {
-  versions: ["1.0.1", "1.1.0-alpha", "2.0.0-beta1"],
   navMain: [
     {
-      title: "Recent Speechs",
+      title: "Recent Speeches",
       url: "#",
       items: [
         {
@@ -47,6 +48,7 @@ const data = {
         {
           title: "College Tribute",
           url: "#",
+          isActive: true,
         },
       ],
     },
@@ -55,20 +57,11 @@ const data = {
       url: "#",
       items: [
         {
-          title: "Routing",
+          title: "Subscription",
           url: "#",
         },
         {
-          title: "Data Fetching",
-          url: "#",
-          isActive: true,
-        },
-        {
-          title: "Authentication",
-          url: "#",
-        },
-        {
-          title: "Deploying",
+          title: "Settings",
           url: "#",
         },
       ],
@@ -78,79 +71,81 @@ const data = {
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar {...props}>
-      <SidebarHeader>
-        <div className="w-full flex items-center justify-between px-2 py-2">
-          <Link href={"/"}>
-            <Image src={logo} alt="logo" width={90} />
-          </Link>
-        </div>
-      </SidebarHeader>
-      <SidebarContent className="gap-0">
-        {/* We create a collapsible SidebarGroup for each parent. */}
-        {data.navMain.map((item) => (
-          <Collapsible
-            key={item.title}
-            title={item.title}
-            defaultOpen
-            className="group/collapsible"
-          >
-            <SidebarGroup>
-              <SidebarGroupLabel
-                asChild
-                className="group/label text-sidebar-foreground hover:bg-sidebar-accent hover:text-sidebar-accent-foreground text-sm"
-              >
-                <CollapsibleTrigger>
-                  {item.title}{" "}
-                  <ChevronRight className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-90" />
-                </CollapsibleTrigger>
-              </SidebarGroupLabel>
-              <CollapsibleContent>
-                <SidebarGroupContent>
-                  <SidebarMenu>
-                    {item.items.map((item) => (
-                      <SidebarMenuItem key={item.title}>
-                        <SidebarMenuButton asChild isActive={item.isActive}>
-                          <a href={item.url}>{item.title}</a>
-                        </SidebarMenuButton>
-                      </SidebarMenuItem>
-                    ))}
-                  </SidebarMenu>
-                </SidebarGroupContent>
-              </CollapsibleContent>
-            </SidebarGroup>
-          </Collapsible>
-        ))}
-      </SidebarContent>
-      <SidebarFooter>
-        <Card className="shadow-none">
-          <form>
-            <CardHeader className="p-4 pb-0">
-              <CardTitle className="text-sm">
-                Subscribe to our newsletter
-              </CardTitle>
-              <CardDescription>
-                Opt-in to receive updates and news about the sidebar.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="grid gap-2.5 p-4">
-              <div className="flex items-center gap-2">
-                <Progress max={10} value={5} />
-                <p className="flex items-center text-xs">
-                  <span>5</span> / <span>10</span>
-                </p>
-              </div>
-              <Button
-                className="w-full bg-sidebar-primary text-sidebar-primary-foreground shadow-none"
-                size="sm"
-              >
-                Upgrade
-              </Button>
-            </CardContent>
-          </form>
-        </Card>
-      </SidebarFooter>
-      <SidebarRail />
-    </Sidebar>
+    <StoreProvider>
+      <Sidebar {...props}>
+        <SidebarHeader>
+          <div className="w-full flex items-center justify-between px-2 py-2">
+            <Link href={"/"}>
+              <Image src={logo} alt="logo" width={90} />
+            </Link>
+          </div>
+        </SidebarHeader>
+        <SidebarContent className="gap-0">
+          {/* We create a collapsible SidebarGroup for each parent. */}
+          {data.navMain.map((item) => (
+            <Collapsible
+              key={item.title}
+              title={item.title}
+              defaultOpen
+              className="group/collapsible"
+            >
+              <SidebarGroup>
+                <SidebarGroupLabel
+                  asChild
+                  className="group/label text-sidebar-foreground hover:bg-sidebar-accent hover:text-sidebar-accent-foreground text-sm"
+                >
+                  <CollapsibleTrigger>
+                    {item.title}{" "}
+                    <ChevronRight className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-90" />
+                  </CollapsibleTrigger>
+                </SidebarGroupLabel>
+                <CollapsibleContent>
+                  <SidebarGroupContent>
+                    <SidebarMenu>
+                      {item.items.map((item) => (
+                        <SidebarMenuItem key={item.title}>
+                          <SidebarMenuButton asChild isActive={item.isActive}>
+                            <a href={item.url}>{item.title}</a>
+                          </SidebarMenuButton>
+                        </SidebarMenuItem>
+                      ))}
+                    </SidebarMenu>
+                  </SidebarGroupContent>
+                </CollapsibleContent>
+              </SidebarGroup>
+            </Collapsible>
+          ))}
+        </SidebarContent>
+        <SidebarFooter>
+          <Card className="shadow-none">
+            <form>
+              <CardHeader className="p-4 pb-0">
+                <CardTitle className="text-sm">
+                  Subscribe to our newsletter
+                </CardTitle>
+                <CardDescription>
+                  Opt-in to receive updates and news about the sidebar.
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="grid gap-2.5 p-4">
+                <div className="flex items-center gap-2">
+                  <Progress max={10} value={5} />
+                  <p className="flex items-center text-xs">
+                    <span>5</span> / <span>10</span>
+                  </p>
+                </div>
+                <Button
+                  className="w-full bg-sidebar-primary text-sidebar-primary-foreground shadow-none"
+                  size="sm"
+                >
+                  Upgrade
+                </Button>
+              </CardContent>
+            </form>
+          </Card>
+        </SidebarFooter>
+        <SidebarRail />
+      </Sidebar>
+    </StoreProvider>
   );
 }
