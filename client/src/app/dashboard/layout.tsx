432e81304921c569ae758f53606bbf2b001263df
sandeep <sandeepshome10@gmail.com>
2025-05-17 19:21:03 +0530

    Chore: add redux store provider

--- client/src/app/dashboard/layout.tsx
+++ client/src/app/dashboard/layout.tsx
@@ -1,20 +1,25 @@
+"use client";
+
 import AppHeader from "@/components/app-header";
 import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import StoreProvider from "@/lib/store-provider";
 
 const DashboardLayout = ({
   children,
 }: Readonly<{ children: React.ReactNode }>) => {
   return (
-    <div className="">
-      <SidebarProvider>
-        <AppSidebar />
-        <SidebarInset>
-          <AppHeader />
-          <main className="p-4">{children}</main>
-        </SidebarInset>
-      </SidebarProvider>
-    </div>
+    <StoreProvider>
+      <div className="">
+        <SidebarProvider>
+          <AppSidebar />
+          <SidebarInset>
+            <AppHeader />
+            <main className="p-4">{children}</main>
+          </SidebarInset>
+        </SidebarProvider>
+      </div>
+    </StoreProvider>
   );
 };
 
