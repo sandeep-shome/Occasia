3e69c76f1127ba708aeeb8c7efa49632868ee903
sandeep <sandeepshome10@gmail.com>
2025-05-18 20:40:55 +0530

    feat: add ai speech generation hook

---
+++ client/src/hooks/use-generate.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { useState } from "react";
+
+export const useGenerate = () => {
+  const [pending, setPending] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  async function getSpeech(speechId: string) {
+    setPending(true);
+    try {
+      const res = await axios.get(`/api/generation/ai/${speechId}`);
+      return res.data;
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+      } else {
+        setError("Error: something went wrong!");
+      }
+    } finally {
+      setPending(false);
+    }
+  }
+
+  return { pending, error, getSpeech };
+};
