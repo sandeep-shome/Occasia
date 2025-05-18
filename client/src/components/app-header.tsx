40f5226de5238dd773c8bb4d596ea5ef94053249
sandeep <sandeepshome10@gmail.com>
2025-05-18 14:42:09 +0530

    Chore: add dynamic breadcrumbs 🚧

--- client/src/components/app-header.tsx
+++ client/src/components/app-header.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import {
   Breadcrumb,
@@ -10,8 +12,10 @@ import {
 import { Separator } from "@/components/ui/separator";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { usePathname } from "next/navigation";
 
 function AppHeader() {
+  const pathname = usePathname().split("/");
   return (
     <>
       <header className="flex justify-between sticky top-0 bg-background h-16 shrink-0 items-center gap-2 border-b px-4">
@@ -20,15 +24,16 @@ function AppHeader() {
           <Separator orientation="vertical" className="mr-2 h-4" />
           <Breadcrumb>
             <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">
-                  Building Your Application
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block" />
-              <BreadcrumbItem>
-                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-              </BreadcrumbItem>
+              {pathname.map((path, index) => (
+                <>
+                  <BreadcrumbItem className="hidden md:block">
+                    <BreadcrumbLink href="#">{path}</BreadcrumbLink>
+                  </BreadcrumbItem>
+                  {index != pathname.length - 1 && (
+                    <BreadcrumbSeparator className="hidden md:block" />
+                  )}
+                </>
+              ))}
             </BreadcrumbList>
           </Breadcrumb>
         </div>
