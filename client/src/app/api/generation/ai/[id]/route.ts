4ece989bfb13d53fd963e05c31b7676aff322a0d
sandeep <sandeepshome10@gmail.com>
2025-05-18 19:25:07 +0530

    chore: add security and token deduction

--- client/src/app/api/generation/ai/[id]/route.ts
+++ client/src/app/api/generation/ai/[id]/route.ts
@@ -20,6 +20,19 @@ export async function GET(
       );
     }
 
+    if (speechData.regenerationCount > 0) {
+      // Sending error if it's a regeneration request
+      return NextResponse.json(
+        { message: "Error: too much tries" },
+        { status: 405 }
+      );
+    }
+
+    if (speechData.isFailed) {
+      // Sending error if it's a failed request
+      return NextResponse.json({ message: "Error: failed" }, { status: 418 });
+    }
+
     // implementing ai generation
     const {
       name,
@@ -34,13 +47,37 @@ export async function GET(
       ...payloadData
     } = speechData;
     const message = await generateMessage(payloadData);
-    return NextResponse.json({ message: message.content });
+
+    // storing speech data
+
+    const updatedSpeechData = await prisma.speech.update({
+      where: {
+        id: speechId,
+      },
+      data: {
+        result: message.content,
+      },
+    });
+
+    // deducting user token
+    await prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        tokens: {
+          decrement: 1,
+        },
+      },
+    });
+
+    return NextResponse.json(updatedSpeechData);
   } catch (error) {
     if (error instanceof Error) {
-      return NextResponse.json({ error: error.message }, { status: 405 });
+      return NextResponse.json({ message: error.message }, { status: 405 });
     } else {
       return NextResponse.json(
-        { error: "Internal server error" },
+        { message: "Internal server error" },
         { status: 500 }
       );
     }
