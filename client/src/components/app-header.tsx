2eabd961ca2b9e12892c6aade1abb4437cb6efc7
sandeep <sandeepshome10@gmail.com>
2025-05-18 22:12:23 +0530

    Chore: remove breadcrumbs

--- client/src/components/app-header.tsx
+++ client/src/components/app-header.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React from "react";
 import {
   Breadcrumb,
@@ -15,32 +13,14 @@ import { SidebarTrigger } from "@/components/ui/sidebar";
 import { usePathname } from "next/navigation";
 
 function AppHeader() {
-  const pathname = usePathname().split("/");
   return (
     <>
       <header className="flex justify-between sticky top-0 bg-background h-16 shrink-0 items-center gap-2 border-b px-4">
         <div className="flex items-center gap-2">
           <SidebarTrigger className="-ml-1" />
           <Separator orientation="vertical" className="mr-2 h-4" />
-          <Breadcrumb>
-            <BreadcrumbList>
-              {pathname.map((path, index) => (
-                <div key={index}>
-                  <BreadcrumbItem className="hidden md:block">
-                    <BreadcrumbLink href="#">{path}</BreadcrumbLink>
-                  </BreadcrumbItem>
-                  {index != pathname.length - 1 && (
-                    <BreadcrumbSeparator className="hidden md:block" />
-                  )}
-                </div>
-              ))}
-            </BreadcrumbList>
-          </Breadcrumb>
         </div>
         <div className="">
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
           <SignedIn>
             <UserButton />
           </SignedIn>
