24a08e370f7f1f6f4772f12a059e85075533dd3c
sandeep <sandeepshome10@gmail.com>
2025-05-20 12:45:44 +0530

    chore: add mock error route

--- client/src/app/api/mock/route.ts
+++ client/src/app/api/mock/route.ts
@@ -1,6 +1,9 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   const id = "f27b8g398hf23p90fh9";
-  return new Response(JSON.stringify({ id }), { status: 201 });
+  return NextResponse.json(
+    { message: "error: some error occurred!" },
+    { status: 400 }
+  );
 }
