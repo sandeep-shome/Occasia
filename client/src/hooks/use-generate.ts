3eb1e4900f4e8daf49e7e38281b5ea39f8cb83f7
sandeep <sandeepshome10@gmail.com>
2025-05-21 23:02:11 +0530

    refactor: change catch & response data

--- client/src/hooks/use-generate.ts
+++ client/src/hooks/use-generate.ts
@@ -1,22 +1,24 @@
-import { SpeechData } from "@/types";
 import axios from "axios";
 import { useState } from "react";
 
 export const useGenerate = () => {
   const [pending, setPending] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
+  const [error, setError] = useState<{
+    message: string;
+    status: number;
+  } | null>(null);
 
   async function getSpeech(speechId: string) {
     setPending(true);
+    setError(null);
     try {
       const res = await axios.get(`/api/generation/ai/${speechId}`);
-      return res.data as SpeechData;
-    } catch (error) {
-      if (error instanceof Error) {
-        setError(error.message);
-      } else {
-        setError("Error: something went wrong!");
-      }
+      return res;
+    } catch (error: any) {
+      setError({
+        message: error.response.data.message,
+        status: error.response.status,
+      });
     } finally {
       setPending(false);
     }
