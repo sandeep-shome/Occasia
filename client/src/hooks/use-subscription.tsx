00fe1e7a7725307d164392e040fadcb8b0e10024
sandeep <sandeepshome10@gmail.com>
2025-05-21 19:54:02 +0530

    chore: returning axios response

--- client/src/hooks/use-subscription.tsx
+++ client/src/hooks/use-subscription.tsx
@@ -1,32 +1,21 @@
 import axios, { AxiosResponse } from "axios";
 import { useState } from "react";
 
-interface Subscription {
-  id: String;
-  tokens: number;
-  userId: String;
-  amount: number;
-  successful: Boolean;
-  createdAt: Date;
-}
-
 export const useSubscription = () => {
   const [error, setError] = useState<{
     message: string;
     status: number;
   } | null>(null);
   const [pending, setPending] = useState<boolean>(true);
-  const [data, setData] = useState<Subscription[] | null>(null);
 
   const getSubscriptions = async (userId: string) => {
-    setData(null);
     setError(null);
     setPending(true);
     try {
       const res: AxiosResponse = await axios.get(
         `/api/fetch/subscription/${userId}`
       );
-      setData(res.data);
+      return res;
     } catch (error: any) {
       setError({
         message: error.response.data.message,
@@ -37,5 +26,5 @@ export const useSubscription = () => {
     }
   };
 
-  return { pending, error, data, getSubscriptions };
+  return { pending, error, getSubscriptions };
 };
