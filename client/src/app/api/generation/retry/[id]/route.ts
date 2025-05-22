f8c4d3058f25777368b275102305a93ee036bcd7
sandeep <sandeepshome10@gmail.com>
2025-05-22 13:10:13 +0530

    chore: add retry route

---
+++ client/src/app/api/generation/retry/[id]/route.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  try {
+    const { id } = await params;
+  } catch (err) {
+    if (err instanceof Error) {
+      return NextResponse.json({ message: err.message }, { status: 400 });
+    }
+    return NextResponse.json(
+      { message: "Something went wrong!" },
+      { status: 500 }
+    );
+  }
+};
