771db9f804e8b528afd93438389ef3692d2b45fe
sandeep <sandeepshome10@gmail.com>
2025-05-22 13:58:52 +0530

    chore: add toast for notification

--- client/src/components/copy-button.tsx
+++ client/src/components/copy-button.tsx
@@ -3,11 +3,13 @@
 import React, { useState } from "react";
 import { Button } from "./ui/button";
 import { Clipboard, ClipboardCheck } from "lucide-react";
+import { toast } from "sonner";
 
 const CopyButton = ({ data }: { data: string }) => {
   const [copied, setCopied] = useState<boolean>(false);
   const handleCopy = () => {
     setCopied(true);
+    toast.info("Copied");
     window.navigator.clipboard.writeText(data);
     setTimeout(() => setCopied(false), 2000);
   };
