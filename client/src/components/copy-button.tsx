6c38cc36be2bfb9fc31d7a8ceed99e28138d4082
sandeep <sandeepshome10@gmail.com>
2025-05-22 13:46:40 +0530

    chore: add copy button

---
+++ client/src/components/copy-button.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React, { useState } from "react";
+import { Button } from "./ui/button";
+import { Clipboard, ClipboardCheck } from "lucide-react";
+
+const CopyButton = (data: string) => {
+  const [copied, setCopied] = useState<boolean>(false);
+  const handleCopy = () => {
+    setCopied(true);
+    window.navigator.clipboard.writeText(data);
+    setTimeout(() => setCopied(false), 2000);
+  };
+
+  return (
+    <Button variant={"outline"} size={"icon"} onClick={handleCopy}>
+      {copied ? (
+        <ClipboardCheck className="size-4 text-neutral-600" />
+      ) : (
+        <Clipboard className="size-4 text-neutral-600" />
+      )}
+    </Button>
+  );
+};
+
+export default CopyButton;
