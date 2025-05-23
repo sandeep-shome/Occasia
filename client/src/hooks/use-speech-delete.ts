2237ac34b78eb5d98ade5800b7650da97f82cc88
sandeep <sandeepshome10@gmail.com>
2025-05-24 00:15:12 +0530

    chore: add initial speech delete route

---
+++ client/src/hooks/use-speech-delete.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { useState } from "react";
+
+export const useSpeechDelete = () => {
+  const [error, setError] = useState<{
+    message: string;
+    status: number;
+  } | null>(null);
+  const [pending, setPending] = useState<boolean>(false);
+
+  async function deleteSpeech(
+    speechId: string,
+    userId: string,
+    updatedResult: string
+  ) {
+    setPending(true);
+    setError(null);
+    try {
+      const res = await axios.delete(
+        `/api/speech/delete/${userId}/${speechId}`
+      );
+      return res;
+    } catch (error: any) {
+      setError({
+        message: error.response.data.message,
+        status: error.response.status,
+      });
+    } finally {
+      setPending(false);
+    }
+  }
+
+  return { deleteSpeech, pending, error };
+};
