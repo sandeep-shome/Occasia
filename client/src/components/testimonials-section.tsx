b77a040ef83dc1694c27c86ffd6563e8eb9500b8
sandeep <sandeepshome10@gmail.com>
2025-06-07 20:52:26 +0530

    refactor: finalized & cleaned up - v1.0.0 release ♻️

--- client/src/components/testimonials-section.tsx
+++ client/src/components/testimonials-section.tsx
@@ -1,47 +1,9 @@
 import { cn } from "@/lib/utils";
 import React from "react";
 import { Marquee } from "./magicui/marquee";
+import reviews from "@/data/reviews.json";
 
 const TestimonialsSction = () => {
-  const reviews = [
-    {
-      name: "Jack",
-      username: "@jack",
-      body: "I've never seen anything like this before. It's amazing. I love it.",
-      img: "https://avatar.vercel.sh/jack",
-    },
-    {
-      name: "Jill",
-      username: "@jill",
-      body: "I don't know what to say. I'm speechless. This is amazing.",
-      img: "https://avatar.vercel.sh/jill",
-    },
-    {
-      name: "John",
-      username: "@john",
-      body: "I'm at a loss for words. This is amazing. I love it.",
-      img: "https://avatar.vercel.sh/john",
-    },
-    {
-      name: "Jane",
-      username: "@jane",
-      body: "I'm at a loss for words. This is amazing. I love it.",
-      img: "https://avatar.vercel.sh/jane",
-    },
-    {
-      name: "Jenny",
-      username: "@jenny",
-      body: "I'm at a loss for words. This is amazing. I love it.",
-      img: "https://avatar.vercel.sh/jenny",
-    },
-    {
-      name: "James",
-      username: "@james",
-      body: "I'm at a loss for words. This is amazing. I love it.",
-      img: "https://avatar.vercel.sh/james",
-    },
-  ];
-
   const firstRow = reviews.slice(0, reviews.length / 2);
   const secondRow = reviews.slice(reviews.length / 2);
 
