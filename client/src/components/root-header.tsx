5433c2d4a133c96cea0c5b196b51545b204e3471
sandeep <sandeepshome10@gmail.com>
2025-06-07 12:04:57 +0530

    feat: add nav for mobile

--- client/src/components/root-header.tsx
+++ client/src/components/root-header.tsx
@@ -1,15 +1,21 @@
 "use client";
-
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import { Menu } from "lucide-react";
+// FIXME: Refactor
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { Menu, XIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { ModeToggle } from "./theme-toogler";
 import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
+import { ShinyButton } from "./magicui/shiny-button";
+import { useRouter } from "next/navigation";
 
 const RootHeader = () => {
   const [isScrolled, setIsScrolled] = useState<boolean>(false);
+  const [currentPathname, setCurrentPathname] = useState<string>("#about");
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState<boolean>(false);
+  const router = useRouter();
 
   const handleScroll = (): void => {
     window.scrollY > 0 ? setIsScrolled(true) : setIsScrolled(false);
@@ -20,10 +26,34 @@ const RootHeader = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+  const data = [
+    {
+      title: "about",
+      url: "#about",
+    },
+    {
+      title: "pricing",
+      url: "#pricing",
+    },
+    {
+      title: "testimonials",
+      url: "#testimonials",
+    },
+    {
+      title: "privacy",
+      url: "#privacy",
+    },
+    {
+      title: "help",
+      url: "#help",
+    },
+  ];
+
+  //FIXME: navlink active toggle
   return (
     <header
       className={cn(
-        "bg-white dark:bg-background border-b-1 fixed top-0 left-0 w-full",
+        "bg-white dark:bg-background border-b-1 fixed top-0 left-0 w-full z-50",
         isScrolled ? "" : "border-transparent"
       )}
     >
@@ -32,72 +62,35 @@ const RootHeader = () => {
           <div className="md:flex md:items-center md:gap-12">
             <Link className="block" href="#">
               <span className="sr-only">Home</span>
-              <Image src={"logo.svg"} alt="logo" width={100} height={40} />
+              <Image
+                src={"logo.svg"}
+                alt="logo"
+                width={100}
+                height={40}
+                className="dark:invert-100"
+              />
             </Link>
           </div>
 
           <div className="hidden md:block">
             <nav aria-label="Global">
               <ul className="flex items-center gap-6 text-sm">
-                <li>
-                  <a
-                    className="text-primary transition hover:text-primary/75"
-                    href="#"
-                  >
-                    {" "}
-                    About{" "}
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className="text-gray-500 transition hover:text-gray-500/75"
-                    href="#"
-                  >
-                    {" "}
-                    Careers{" "}
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className="text-gray-500 transition hover:text-gray-500/75"
-                    href="#"
-                  >
-                    {" "}
-                    History{" "}
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className="text-gray-500 transition hover:text-gray-500/75"
-                    href="#"
-                  >
-                    {" "}
-                    Services{" "}
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className="text-gray-500 transition hover:text-gray-500/75"
-                    href="#"
-                  >
-                    {" "}
-                    Projects{" "}
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className="text-gray-500 transition hover:text-gray-500/75"
-                    href="#"
-                  >
-                    {" "}
-                    Blog{" "}
-                  </a>
-                </li>
+                {data.map((nav, index) => (
+                  <li key={index}>
+                    <Link
+                      className={cn(
+                        "hover:text-primary/80 transition dark:hover:text-primary/75 capitalize",
+                        nav.url === currentPathname
+                          ? "text-primary/80 dark:text-primary/75"
+                          : "text-primary/60 dark:text-primary/40"
+                      )}
+                      href={nav.url}
+                      onClick={() => setCurrentPathname(nav.url)}
+                    >
+                      {nav.title}
+                    </Link>
+                  </li>
+                ))}
               </ul>
             </nav>
           </div>
@@ -110,17 +103,56 @@ const RootHeader = () => {
                 <UserButton />
               </SignedIn>
               <SignedOut>
-                <SignInButton />
+                <ShinyButton onClick={() => router.push("/dashboard")}>
+                  Sign in
+                </ShinyButton>
               </SignedOut>
             </div>
 
             <div className="block md:hidden">
-              <button className="rounded-sm bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
-                <Menu className="size-4" />
-              </button>
+              <Button
+                variant={"ghost"}
+                size={"icon"}
+                onClick={() => setIsMobileNavOpen((prev) => !prev)}
+                className="transition"
+              >
+                {isMobileNavOpen ? (
+                  <XIcon className="size-5 text-destructive" />
+                ) : (
+                  <Menu className="size-5" />
+                )}
+              </Button>
             </div>
           </div>
         </div>
+      </div>
+
+      <div
+        className={cn(
+          "fixed top-0 left-0 w-[70%] h-screen z-50 bg-background border-e items-center justify-center md:hidden transition-all",
+          isMobileNavOpen ? "flex" : "hidden"
+        )}
+      >
+        <nav aria-label="Global">
+          <ul className="flex items-center flex-col gap-6 text-sm">
+            {data.map((nav, index) => (
+              <li key={index}>
+                <Link
+                  className={cn(
+                    "hover:text-primary/80 transition dark:hover:text-primary/75 capitalize",
+                    nav.url === currentPathname
+                      ? "text-primary/80 dark:text-primary/75"
+                      : "text-primary/60 dark:text-primary/40"
+                  )}
+                  href={nav.url}
+                  onClick={() => setCurrentPathname(nav.url)}
+                >
+                  {nav.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
     </header>
   );
